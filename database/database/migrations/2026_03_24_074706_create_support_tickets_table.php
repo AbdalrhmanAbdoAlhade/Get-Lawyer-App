353b1ce80167bb46d29b43a5ab8df6a05dcec6d1
Abdalrhman AbdoAlhade <161177575+AbdalrhmanAbdoAlhade@users.noreply.github.com>
2026-03-28 10:38:36 +0200

    Add files via upload

---
+++ database/database/migrations/2026_03_24_074706_create_support_tickets_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('support_tickets', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained()->cascadeOnDelete();
+            $table->enum('problem_type', [
+                'technical',      // مشكلة تقنية
+                'financial',      // مشكلة مالية
+                'legal',          // مشكلة قانونية
+                'other'           // أخرى
+            ])->default('technical');
+            $table->string('title');
+            $table->string('note');
+            $table->text('description');
+            $table->enum('status', ['pending', 'in_progress', 'resolved', 'closed'])->default('pending');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('support_tickets');
+    }
+};
